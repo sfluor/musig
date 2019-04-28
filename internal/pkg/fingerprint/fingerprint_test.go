6f16abf0303a8959eb560e890118e5f27a376d38
Sami Tabet <sami.tabet@datadoghq.com>
2019-04-28 12:59:32 -0400

    fix: remove print in test

--- internal/pkg/fingerprint/fingerprint_test.go
+++ internal/pkg/fingerprint/fingerprint_test.go
@@ -1,7 +1,6 @@
 package fingerprint
 
 import (
-	"fmt"
 	"os"
 	"path"
 	"testing"
@@ -50,7 +49,6 @@ func testFingerprintingOnFile(t *testing.T, path string) {
 	table := f.Fingerprint(0, cMap)
 	subTable := f.Fingerprint(0, subCMap)
 
-	fmt.Printf("table = %+v\n", table)
 	for key := range subTable {
 		assert.Contains(t, table, key)
 	}
